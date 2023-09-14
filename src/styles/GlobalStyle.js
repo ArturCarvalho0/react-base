3bcedf9c5d0bbbeb7418b78eb34a55f4b873e934
Artur Carvalho <artur.carvalho0@outlook.com>
2023-09-14 13:46:21 -0300

    Configurando React Toastify

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import { primaryColor, primaryDarkColor } from '../config/colors';
+import * as colors from '../config/colors';
+import 'react-toastify/dist/ReactToastify.css'
 
 export default createGlobalStyle`
   * {
@@ -11,8 +12,8 @@ export default createGlobalStyle`
 
   body {
     font-family: sans-serif;
-    background: ${primaryDarkColor};
-    color: ${primaryDarkColor};
+    background: ${colors.primaryDarkColor};
+    color: ${colors.primaryDarkColor};
   }
 
   html, body, #root {
@@ -20,7 +21,7 @@ export default createGlobalStyle`
   }
   button {
     cursor: pointer;
-    background: ${primaryColor};
+    background: ${colors.primaryColor};
     border: none;
     color: #fff;
     padding: 10px 20px;
@@ -30,7 +31,7 @@ export default createGlobalStyle`
 
   a {
     text-decoration: none;
-    color: ${primaryColor};
+    color: ${colors.primaryColor};
   }
   ul {
     list-style: none;
